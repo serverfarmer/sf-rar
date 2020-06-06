44efa9e03c10b6283f2bbfbe8c15eb35f9b314d2
Tomasz Klim <github@tomaszklim.pl>
2020-06-06 19:05:43 +0200

    fix for rar version detection

--- get-rar-version.sh
+++ get-rar-version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -sS https://rarlab.com |grep "WinRAR and RAR .... release" |egrep -o '[0-9]{1}\.[0-9]{1,3}' |sed -e s/[.]//g -e 's/./&./g' -e s/[.]$//g
+curl -sS https://www.rarlab.com |grep "WinRAR and RAR .... release" |egrep -o '[0-9]{1}\.[0-9]{1,3}' |sed -e s/[.]//g -e 's/./&./g' -e s/[.]$//g
